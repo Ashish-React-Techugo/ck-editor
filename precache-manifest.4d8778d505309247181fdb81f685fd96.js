96e3aae9839de8fa3c421e307c6d104af364415f
Ashish-Techugo <ashish.bisht@techugo.com>
2022-09-29 16:47:59 +0530

    Updates

---
+++ precache-manifest.4d8778d505309247181fdb81f685fd96.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1455738e42589afc173",
+    "url": "/ck-editor/static/js/main.c1455738.chunk.js"
+  },
+  {
+    "revision": "4138d9607b4f87305ef0",
+    "url": "/ck-editor/static/css/1.7c3825b7.chunk.css"
+  },
+  {
+    "revision": "4138d9607b4f87305ef0",
+    "url": "/ck-editor/static/js/1.4138d960.chunk.js"
+  },
+  {
+    "revision": "b83921ad3e897e693592",
+    "url": "/ck-editor/static/js/runtime~main.b83921ad.js"
+  },
+  {
+    "revision": "7e263200d01fb5998c401b03ef3e6433",
+    "url": "/ck-editor/static/media/summernote.7e263200.eot"
+  },
+  {
+    "revision": "03f45d882ff3a80b719cfc0789c5eab9",
+    "url": "/ck-editor/static/media/summernote.03f45d88.woff"
+  },
+  {
+    "revision": "549304631a5f1ab69498e294543086e3",
+    "url": "/ck-editor/static/media/summernote.54930463.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/ck-editor/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/ck-editor/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/ck-editor/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/ck-editor/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/ck-editor/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "2ba7dced6d976aff753e3d2038a32bb3",
+    "url": "/ck-editor/index.html"
+  }
+];
